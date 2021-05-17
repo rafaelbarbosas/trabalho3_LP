d9800beb457f746798670114b0b4515bed083640
Rafael Barbosa <rafael.1940.b@gmail.com>
2021-05-17 20:22:18 -0300

    Adiciona metodos da classe de contas bonus

--- src/ContaBonificada.java
+++ src/ContaBonificada.java
@@ -1,9 +1,21 @@
 class ContaBonificada extends Conta {
+    private double bonus;
+
     public ContaBonificada(String numero, Cliente cliente){
         super(numero, cliente);
     }
 
-    public int getBonus() {
-        return 0;
+    public void creditar(double valor){
+        bonus = bonus + (valor * 0.01);
+        super.creditar(valor);
+    }
+
+    public void renderBonus() {
+        super.creditar(bonus);
+        bonus = 0;
+    }
+
+    public double getBonus() {
+        return bonus;
     }
 }
